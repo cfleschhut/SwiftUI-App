e56eb55803de1d239409c497a206afa207f440fc
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-06-26 12:48:19 +0200

    Initial Commit

---
+++ HelloSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloSwift
+//
+//  Created by Christian Fleschhut on 26.06.20.
+//  Copyright © 2020 Christian Fleschhut. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
